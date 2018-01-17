da6487444c2dc2ae8453a3898af0226ed8c0af7e
LosCojones <alber_1996@hotmail.com>
2018-01-17 17:40:23 +0000

    now

--- app/models/puja.rb
+++ app/models/puja.rb
@@ -3,4 +3,6 @@ class Puja < ApplicationRecord
   belongs_to :subasta,class_name: 'Subastum'
 
   validates :user, :subasta, :fecha_puja, :valor, presence: true
+
+  scope :pujasubasta, -> (id){where('subastaid = ?', id)}
 end
